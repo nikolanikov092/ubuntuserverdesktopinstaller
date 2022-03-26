d85f86dda9826348aa68dfa607ff591af168a144
nikolanikov092 <94351312+nikolanikov092@users.noreply.github.com>
2022-03-26 11:59:48 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo Ubuntu Server Desktop Environment Installer
+echo Packages: LXQT, LightDM, VLC, Xorg, GNote, GParted, LibreOffice, GNOME Games, Rhythmbox, Firefox ESR
+apt install xserver-xorg -y
+apt install lightdm -y
+apt install lxqt -y
+apt install vlc -y
+apt install gnote -y
+apt install gparted -y
+apt install libreoffice -y
+apt install gnome-games -y
+apt install rhythmbox -y 
+apt install firefox-esr -y
+echo "           _ _   _____                   _  "
+echo "     /\   | | | |  __ \                 | | "
+echo "    /  \  | | | | |  | | ___  _ __   ___| | "
+echo "   / /\ \ | | | | |  | |/ _ \| '_ \ / _ \ | "
+echo "  / ____ \| | | | |__| | (_) | | | |  __/_| "
+echo " /_/    \_\_|_| |_____/ \___/|_| |_|\___(_) "
+echo "                                            "
+echo "Designed by Nikola Nikov in Bulgaria"
